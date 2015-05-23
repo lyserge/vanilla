4e50855f8b7164a664aa00bb69274ea0f182721e
alexbridge <alexb@cogapp.com>
2015-05-23 15:55:32 +0100

    Added GB locale

---
+++ locales/vf_en_GB/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_en_GB'] = array (
+  'Locale' => 'en_GB',
+  'Name' => 'English (United Kingdom)',
+  'Description' => 'Official English (United Kingdom) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/en_GB/">here</a>.',
+  'Version' => '2015.05.22p1218',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/en_GB/',
+);
